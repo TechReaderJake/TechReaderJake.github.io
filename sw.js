a9a2c10050b5bd0756fad6aab869838c06e1629f
Jacob Ashcraft <masterofallreaders@gmail.com>
2019-09-20 19:54:36 -0700

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-8e68a4975aa25391c809.js"
+    "url": "webpack-runtime-04ebbf8054d16ea0f223.js"
   },
   {
     "url": "styles.bfc1cf8355a8e51bda5a.css"
@@ -42,24 +42,24 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "4e4c9cc6461ea56fc4c2bb73a8118819"
+    "revision": "f1edf7e78a4cf4e29eac2d5e8903f3f0"
   },
   {
-    "url": "component---src-pages-404-js-754b628cd48048e81384.js"
+    "url": "component---src-pages-404-js-0978c02afd377e15a6bb.js"
   },
   {
-    "url": "1-0abb1a9d084301bb61f5.js"
+    "url": "1-4abd675cb6368de923ad.js"
   },
   {
     "url": "2-f9f9342c3674c3a2e621.js"
   },
   {
     "url": "page-data/404.html/page-data.json",
-    "revision": "7c34d9c1c6c7f3039d446c0b87633702"
+    "revision": "685d28aa3cb789d0604a66c8cdcb282a"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
-    "revision": "e4d37951ae7c1225bab6a539fc4c857d"
+    "revision": "78214a0820cfc000f41213ff3e6deaed"
   },
   {
     "url": "manifest.webmanifest",
